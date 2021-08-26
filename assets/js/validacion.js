f83de66a7f74f668933af847a9e0585b83a92d58
Maxxy Cabral <20187406@itla.edu.do>
2021-08-25 21:30:22 -0400

    cambio

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -1,28 +1,33 @@
 
-//Validcion de formulario 1
+//Validcion de formulario
+
 function Validacion() {
+  // Aqui validamos el campo de mensajes
   if (document.formulario.mensaje.value.length == 0) {
     alert("Debe escribir un mensaje");
     document.formulario.apellido.focus();
     return false;
   }
+  // Aqui validamos el campo de nombre
 
   if (document.formulario.nombre.value.length == 0) {
     alert("Debe escribir un nombre");
     document.formulario.nombre.focus();
     return false;
   }
+  // Aqui validamos el campo de apellido
   if (document.formulario.apellido.value.length == 0) {
     alert("Debe escribir un apellido");
     document.formulario.apellido.focus();
     return false;
   }
-
+  // Aqui validamos el campo de telefono
   if (document.formulario.telefono.value.length == 0) {
     alert("Debe escribir un Telefono");
     document.formulario.email.focus();
     return false;
   }
+  // Aqui validamos el campo de email
   if (document.formulario.email.value.length == 0) {
     alert("Debe escribir un email");
     document.formulario.email.focus();
@@ -32,36 +37,4 @@ function Validacion() {
   }
 }
 
-//Validcion de formulario 2
-function Validacion() {
-  if (document.formulario.mensaje.value.length == 0) {
-    alert("Debe escribir un mensaje");
-    document.formulario.apellido.focus();
-    return false;
-  }
-
-  if (document.formulario.nombre.value.length == 0) {
-    alert("Debe escribir un nombre");
-    document.formulario.nombre.focus();
-    return false;
-  }
-  if (document.formulario.apellido.value.length == 0) {
-    alert("Debe escribir un apellido");
-    document.formulario.apellido.focus();
-    return false;
-  }
-
-  if (document.formulario.telefono.value.length == 0) {
-    alert("Debe escribir un Telefono");
-    document.formulario.email.focus();
-    return false;
-  }
-  if (document.formulario.email.value.length == 0) {
-    alert("Debe escribir un email");
-    document.formulario.email.focus();
-    return false;
-  } else {
-    alert("Formulario Completado");
-  }
-}
 
